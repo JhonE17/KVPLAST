7c7da09140ca49775431c834849c8446b74ce4e6
JhonE17 <jhonepalacios17@gmail.com>
2023-04-15 07:59:09 -0500

    Add component products with modals

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ export const Modal = () => {
         id='defaultModal'
         tabIndex='-1'
         aria-hidden='true'
-        className='fixed top-0 left-0 right-0 z-50 hidden md:w-full p-4 overflow-x-hidden overflow-y-auto inset-0 h-[calc(100%-1rem)] max-h-full'>
+        className='fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto inset-0 h-[calc(100%-1rem)] max-h-full'>
         <div className='relative w-full max-w-2xl max-h-full'>
           <div className='relative bg-white rounded-lg shadow '>
             <div className='flex items-start justify-between p-4 border-b rounded-t'>
